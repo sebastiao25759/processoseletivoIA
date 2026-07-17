2510607b429085400bbbdec6b230c3bcdb5daaa6
Sebastião <sebastianbach4212@gmail.com>
2026-07-17 14:49:34 -0300

    feat: adiciona implementação do optimize_model.py

--- projetos/1-classificacao-mnist/optimize_model.py
+++ projetos/1-classificacao-mnist/optimize_model.py
@@ -13,3 +13,26 @@ import os
 # ---------------------------------------------------------------------------
 
 # insira seu código aqui
+
+# Carregando o modelo original
+
+model = tf.keras.models.load_model("model.h5")
+
+# Configurando a conversão com otimização (Dynamic Range Quantization)
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+tflite_model = converter.convert()
+
+# Salvando o resultado como "model.tflite"
+with open("model.tflite", "wb") as f:
+    f.write(tflite_model)
+
+
+# Tamanho dos modelos para colocar na documentação
+tflite_model = os.path.getsize("model.tflite") / 1048576
+model_size = os.path.getsize("model.h5") / 1048576
+
+print(f"Tamanho do modelo original: {model_size:.2f} MB")
+print(f"Tamanho do modelo otimizado: {tflite_model:.2f} MB")
